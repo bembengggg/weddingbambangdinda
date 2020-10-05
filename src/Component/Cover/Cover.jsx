6a168f0a1d50b8351ed7ecb8583dc0b2a81febca
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-06 01:27:53 +0700

    ubah btnku

--- src/Component/Cover/Cover.jsx
+++ src/Component/Cover/Cover.jsx
@@ -7,8 +7,7 @@ const Cover =()=>{
         <div className='wrapcover'>
            <img className='img1' src={cover}/>
            <div className='form'>
-            <button className="btnopen" onclick="window.location.href='https://calendar.google.com/event?action=TEMPLATE&tmeid=NTVtbzIzaGpxbXFmZG5kbXE1bXZkNWt0cG1fMjAyMTAxMDlUMDkwMDAwWiBiYW1iYW5nLmt1cm5pYXdhbkB3YW5zb2x1dGlvbnMuY28uaWQ&tmsrc=bambang.kurniawan%40wansolutions.co.id&scp=ALL';">SAVE THE DATE</button>
-            <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MWczYnByYjFvbDhtN2dhMnQxMnNpNjRiMGwgYmVtYmVuZ2dnQG0&tmsrc=bembenggg%40gmail.com">test</a>
+            <button className="btnopen">SAVE THE DATE</button>
             </div>
          </div>
 
