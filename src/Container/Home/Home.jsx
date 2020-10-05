d51bd192fe89067a4e7d0f58e4180cf974b32a17
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-05 16:00:07 +0700

    cssnih

--- src/Container/Home/Home.jsx
+++ src/Container/Home/Home.jsx
@@ -10,7 +10,6 @@ class Home extends Component{
            
             <div className='container'>
                 <Cover/>
-                <Cover/>
                 <NavBottom/>
                 
                 
