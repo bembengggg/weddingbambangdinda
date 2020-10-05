edad7780a00283e0f42e3fddbc14b29a6aaf50dc
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-06 01:20:04 +0700

    ubah btnku

--- src/Component/Cover/Cover.jsx
+++ src/Component/Cover/Cover.jsx
@@ -8,7 +8,7 @@ const Cover =()=>{
            <img className='img1' src={cover}/>
            <div className='form'>
             <button className="btnopen" onclick="window.location.href='https://calendar.google.com/event?action=TEMPLATE&tmeid=NTVtbzIzaGpxbXFmZG5kbXE1bXZkNWt0cG1fMjAyMTAxMDlUMDkwMDAwWiBiYW1iYW5nLmt1cm5pYXdhbkB3YW5zb2x1dGlvbnMuY28uaWQ&tmsrc=bambang.kurniawan%40wansolutions.co.id&scp=ALL';">SAVE THE DATE</button>
-            <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=NzVkYWxyOWVqaGl1czdlYmxnZnQ3NmwybzggYmxrb2M5ZHFvcGs3a2trNm9nNnAzNDlzaDRAZw&amp;tmsrc=blkoc9dqopk7kkk6og6p349sh4%40group.calendar.google.com">test</a>
+            <a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&tmeid=MWczYnByYjFvbDhtN2dhMnQxMnNpNjRiMGwgYmVtYmVuZ2dnQG0&tmsrc=bembenggg%40gmail.com">test</a>
             </div>
          </div>
 
