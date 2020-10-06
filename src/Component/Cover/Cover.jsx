fee4fb6b76d1228b87794edca6344f807926301a
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-06 07:19:05 +0700

    ubah btn

--- src/Component/Cover/Cover.jsx
+++ src/Component/Cover/Cover.jsx
@@ -8,6 +8,9 @@ const Cover =()=>{
            <img className='img1' src={cover}/>
            <div className='form'>
             <button className="btnopen">SAVE THE DATE</button>
+            <a class="to-maps" href="https://www.google.com/maps/place/Sasono+Mulyo+Depok,+Jl.+Raya+Kalimulya+No.30,+Jatimulya,+Kec.+Cilodong,+Kota+Depok,+Jawa+Barat+16413/@-6.4462555,106.8263909,16z/data=!4m2!3m1!1s0x2e69ea1974914827:0xe798a950aa08eae2?hl=id&gl=ID">
+	Direction Maps
+	</a>
             </div>
          </div>
 
