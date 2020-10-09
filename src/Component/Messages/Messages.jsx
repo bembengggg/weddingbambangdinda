402d92cc43ecac4f8ef22ba431e5c3b30180d9db
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-09 17:23:07 +0700

    alert save thedate

--- src/Component/Messages/Messages.jsx
+++ src/Component/Messages/Messages.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Messages.css';
 import messages from '../Messages/bgimage.jpg';
+import protap from '../Messages/protap.jpg';
 
 const Messages =()=>{
     return(
         <div className='wrapcover'>
+            <img className='img2' src={protap}/>
            <img className='img1' src={messages}/>
          </div>
 
