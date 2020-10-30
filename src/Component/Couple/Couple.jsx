6ba75476011d26b9f9412a032a085ab15434b888
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-30 14:32:27 +0700

    text label

--- src/Component/Couple/Couple.jsx
+++ src/Component/Couple/Couple.jsx
@@ -9,7 +9,16 @@ const Couple =()=>{
         <div className='wrapcover'>
            <img className='img1' src={couple}/>
            <div className='htop'>
-                <label className='htopl'>Bismillahirrahmanirrahim</label>
+                <label className='txtb'>Bismillahirrahmanirrahim</label>
+            </div>
+            <div className='htopas'>
+                <label className='txtas'>Assalamu’alaikum warahmatullahi wabarakatuh</label>
+            </div>
+            <div className='htopudg'>
+                <label className='txtudg'>DENGAN MEMOHON RAHMAT ALLAH SWT, KAMI MENGUNDANG</label>
+            </div>
+            <div className='htopbpk'>
+                <label className='txtbpk'>BAPAK/IBU/SAUDARA/i ACARA RESEPSI PERNIKAHAN</label>
             </div>
             <div className='htopb'>
             <img className='htopimg' src={bismillah}/>
