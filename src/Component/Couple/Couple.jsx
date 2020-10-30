6ab11d51876f50764485c321c80007e8ec8b50ef
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-10-30 13:44:49 +0700

    text label

--- src/Component/Couple/Couple.jsx
+++ src/Component/Couple/Couple.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Couple.css';
-import couple from '../Couple/Wedding.jpg';
+// import couple from '../Couple/Wedding.jpg';
+import couple from '../Cover/bgimage.jpg';
+import bismillah from '../../Assets/image/bismillah.png';
 
 const Couple =()=>{
     return(
         <div className='wrapcover'>
            <img className='img1' src={couple}/>
+           <div className='htop'>
+                <label className='htopl'>Bismillahirrahmanirrahim</label>
+            </div>
+            <div className='htopb'>
+            <img className='htopimg' src={bismillah}/>
+            </div>
          </div>
 
 
