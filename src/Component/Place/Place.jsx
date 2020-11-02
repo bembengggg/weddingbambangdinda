20b4f4ea7d79e0ea9d9a3fd083cb4d0c93086927
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-11-03 06:17:51 +0700

    text label

--- src/Component/Place/Place.jsx
+++ src/Component/Place/Place.jsx
@@ -10,6 +10,9 @@ const Place =()=>{
     return(
         <div className='wrapcover'>
            <img className='img2' src={place}/>
+           <div className='bksakad'></div>
+
+
            <div className='form1'>
             <button className="btnopeng" onClick={event =>  window.location.href='https://goo.gl/maps/xvyf6YvwSCZGx3D49'}>View Map</button>
             </div>
