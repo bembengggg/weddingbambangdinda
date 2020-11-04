38657e0c8c27e4971af9d8ec085db2f75e3292a7
bembengggg <beng.1411050084@mail.darmajaya.ac.id>
2020-11-05 00:16:30 +0700

    text label

--- src/Component/Place/Place.jsx
+++ src/Component/Place/Place.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Place.css';
 import place from '../Cover/bgimage.jpg';
+import list from '../../Assets/image/list.png';
 
 const Place =()=>{
 
@@ -26,18 +27,81 @@ const Place =()=>{
            </div>
            <div className='bksrangkaianakaddetail'>
 
-           <div className='bksrangkaianakadborderdetail1'>   
-            
+           <div className='bksrangkaianakadborderdetail1'> 
+                    <div className='bkshari'>
+                        <label className='txthari'>SABTU</label>
+                    </div>
+                    <div className='bkshari'>
+                    <img className='imgharilist' src={list}/>
+                    </div>    
+                    <div className='bkshari'>
+                        <label className='txtpkl'>16.00 S/D 17.00 WIB</label>
+                    </div>  
             </div>
-               <div className='bksrangkaianakaddetail1'>
+               <div className='bksrangkaianakadborderdetail2'>
                     <label className='txttanggal'>09</label>
                </div>
 
-                   <div className='bksrangkaianakadborderdetail2'>
-                   
+                   <div className='bksrangkaianakadborderdetail3'>
+                   <div className='bkshari'>
+                        <label className='txtbulan'>JANUARY</label>
+                    </div>
+                    <div className='bkshari'>
+                    <img className='imgtahunlist' src={list}/>
+                    </div>    
+                    <div className='bkshari'>
+                        <label className='txttahun'>2021</label>
+                    </div> 
                    </div>
            </div>
 
+           <div className='bksrangkaianjalanakad'>
+                <div className='bksrangkaianjalanakad1'><b>SASONO MULYO - DEPOK</b></div>
+           </div>
+           <div className='bksrangkaianjalandetailakad'>
+                <div className='bksrangkaianjalandetailakad1'>Jl. Jatimulya (Kalimulya) No. 30, Cilodong, Depok Jawa Barat</div>
+           </div>
+           
+           <div className='bksrangkaianresepsi'>
+               <div className='bksrangkaianresepsiborder'>   
+                   </div>
+               <div className='bksrangkaianresepsi1'>
+                    <label className='txtresepsi'>Resepsi</label>
+               </div>
+
+                   <div className='bksrangkaianresepsiborder'>
+                   
+                   </div>
+           </div>
+           <div className='bksrangkaianresepsidetail'>
+
+           <div className='bksrangkaianresepsiborderdetail1'> 
+                    <div className='bkshariresepsi'>
+                        <label className='txthariresepsi'>SABTU</label>
+                    </div>
+                    <div className='bkshariresepsi'>
+                    <img className='imgtahunresepsilist' src={list}/>
+                    </div>    
+                    <div className='bkshariresepsi'>
+                        <label className='txtpklresepsi'>19.00 S/D 20.00 WIB</label>
+                    </div>  
+            </div>
+               <div className='bksrangkaianresepsiborderdetail2'>
+                    <label className='txttanggalresepsi'>09</label>
+               </div>
+
+                   <div className='bksrangkaianresepsiborderdetail3'>
+                   <div className='bkshariresepsi'>
+                        <label className='txtbulanresepsi'>JANUARY</label>
+                    </div>
+                    <div className='bkshariresepsi'>
+                    <img className='imgtahunresepsilist' src={list}/>
+                    </div>    
+                    <div className='bkshariresepsi'>
+                        <label className='txttahunresepsi'>2021</label>
+                    </div> 
+                   </div>
+           </div>
 
 
            <div className='form1'>
